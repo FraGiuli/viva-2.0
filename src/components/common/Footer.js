3efbe46d4d6f7e61070cd1292aaf64351963232c
francescoguerrini <francescoguerrini1987@gmail.com>
2024-09-16 13:31:33 +0200

    cookies

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,5 +1,4 @@
 import TextViva from "./TextViva";
-import { Image } from "@nextui-org/react";
 
 export default function Footer() {
   return (
@@ -10,6 +9,25 @@ export default function Footer() {
         <TextViva className="text-sm">
           © 2023 ALICETTE S.R.L. P.IVA 08390270968
         </TextViva>
+      </div>
+      <div className=" text-start">
+        {/* Link alla Privacy Policy */}
+        <a
+          href="https://www.iubenda.com/privacy-policy/38943154"
+          className="iubenda-white iubenda-noiframe iubenda-embed iubenda-noiframe"
+          title="Privacy Policy"
+        >
+          Privacy Policy
+        </a>
+        <span> | </span>
+        {/* Link alla Cookie Policy */}
+        <a
+          href="https://www.iubenda.com/privacy-policy/38943154/cookie-policy"
+          className="iubenda-white iubenda-noiframe iubenda-embed iubenda-noiframe"
+          title="Cookie Policy"
+        >
+          Cookie Policy
+        </a>
       </div>
     </div>
   );
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,15 @@
+// pages/_app.js
 import "@/styles/globals.css";
 import { createTheme, NextUIProvider } from "@nextui-org/react";
 import { Provider } from "react-redux";
 import store from "../redux/store";
+import Script from "next/script"; // Importa il componente Script
 
 export default function App({ Component, pageProps }) {
   const theme = createTheme({
-    type: "light", // it could be "light" or "dark"
+    type: "light", // può essere "light" o "dark"
     theme: {
       colors: {
-        // brand colors
         primaryLight: "#ff73bc",
         primaryLightHover: "#df4a97",
         primaryLightActive: "#ff0286",
@@ -18,22 +19,56 @@ export default function App({ Component, pageProps }) {
         primaryBorderHover: "#df4a97",
         primarySolidHover: "#df4a97",
         primarySolidContrast: "$white",
-
         gradient:
           "linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple500 80%)",
         link: "#ff0286",
-
-        // ...  more colors
+        // ... altri colori
       },
       space: {},
       fonts: {},
     },
   });
+
   return (
-    <Provider store={store}>
-      <NextUIProvider theme={theme}>
-        <Component {...pageProps} />
-      </NextUIProvider>
-    </Provider>
+    <>
+      {/* Configurazione di Iubenda */}
+      <Script id="iubenda-config" strategy="beforeInteractive">
+        {`
+          var _iub = _iub || [];
+          _iub.csConfiguration = {
+            "siteId": 3620215,
+            "cookiePolicyId": 38943154,
+            "lang": "it"
+          };
+        `}
+      </Script>
+
+      {/* Script di autoblocco Iubenda */}
+      <Script
+        strategy="beforeInteractive"
+        src="https://cs.iubenda.com/autoblocking/3620215.js"
+      />
+
+      {/* Script di supporto di Iubenda */}
+      <Script
+        strategy="beforeInteractive"
+        src="https://cdn.iubenda.com/cs/gpp/stub.js"
+      />
+
+      {/* Script principale di Iubenda */}
+      <Script
+        strategy="beforeInteractive"
+        src="https://cdn.iubenda.com/cs/iubenda_cs.js"
+        charSet="UTF-8"
+        async
+      />
+
+      {/* Provider e componenti della tua app */}
+      <Provider store={store}>
+        <NextUIProvider theme={theme}>
+          <Component {...pageProps} />
+        </NextUIProvider>
+      </Provider>
+    </>
   );
 }
