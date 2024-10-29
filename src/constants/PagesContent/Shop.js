bb23422d00958127c9d0923b05bc4b22a31cba5d
GiuliaPuntoni <94111753+GiuliaPuntoni@users.noreply.github.com>
2024-10-29 17:37:28 +0100

    Modifiche shop

--- src/constants/PagesContent/Shop.js
+++ src/constants/PagesContent/Shop.js
@@ -1,13 +1,13 @@
 export const SHOP_CONTENT = {
   it: {
     title: "SHOP",
-    body: "La nostra selezione di prodotti di gastronomia e bottiglie di vino firmati VIVA Viviana Varese, dalle confetture alle creme spalmabili, pesto di pistacchio e giardiniera preparati da **Viviana Varese** e altri prodotti di mare e del nostro Bar Trattoria Vivace **POLPO Semplicemente Pesce**.</br></br>Parole chiave: **gusto, unicità, creatività**! </br>Per ordini e informazioni scrivere a <a href='mailto:shop@vivavivianavarese.it'>shop@vivavivianavarese.it</a>",
+    body: "La nostra selezione di prodotti e pacchi di Natale firmati VIVA Viviana Varese, dalle confetture alle creme spalmabili, dai biscotti al panettone firmato Viviana Varese e i buoni regalo per i nostri ristoranti POLPO, Semplicemente Pesce e FAAK, Cibo e vino a ribellione naturale.</br></br>Parole chiave: **gusto, unicità, creatività**! </br>Per ordini e informazioni scrivere a <a href='mailto:shop@faakfaak.it'>shop@faakfaak.it</a>",
     calltoaction: "> Catalogo di Natale <",
     link: "/shopviva/Catalogo_Natale_2023.pdf",
   },
   en: {
     title: "SHOP",
-    body: "Our selection of gastronomy products and bottles of wine signed by VIVA Viviana Varese, from jams to spreadable creams, pistachio pesto and giardiniera prepared by **Viviana Varese** and other seafood products and our Bar Trattoria Vivace **POLPO Semplicemente Pesce**.</br></br>Keywords: **taste, uniqueness, creativity**! </br>For orders and information write to <a href='mailto:shop@vivavianavarese.it'>shop@vivavianavarese.it</a>",
+    body: "Our selection of Christmas products and packages by VIVA Viviana Varese, from jams to spreads, from biscuits to panettone by Viviana Varese and gift vouchers for our restaurants POLPO, Simply Fish and FAAK, Food and wine with natural rebellion.</br></br>Keywords: **taste, uniqueness, creativity**! </br>For orders and information write to <a href='mailto:shop@faakfaak.it'>shop@faakfaak.it</a>",
     calltoaction: "> Christmas catalogue <",
     link: "/shopviva/Catalogo_Natale_2023.pdf",
   },
